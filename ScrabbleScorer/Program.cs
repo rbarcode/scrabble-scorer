bb8a50372287f410ade418753ec13e9c2d877690
Richard Barbour <richyb07@gmail.com>
2023-09-20 14:39:35 -0700

    Complete basic ui for program.
    Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>

--- ScrabbleScorer/Program.cs
+++ ScrabbleScorer/Program.cs
@@ -1,9 +1,24 @@
+
 using System;
+using ScrabbleScorer.Models;
 
-class Program
+namespace ScrabbleScorer
 {
-  static void Main()
+  class Program
   {
+    static void Main()
+    {
+      Console.WriteLine("Enter a word to check it's score using ScrabbleScorer");
+      string userInput = Console.ReadLine();
+      char[] wordArray = ScoreCalculator.WordToCharArray(userInput);
+      int result = ScoreCalculator.CalcScore(wordArray);
 
+      Console.WriteLine($"The score of {userInput} is : {result}");
+
+      Console.WriteLine("done!");
+
+
+    }
   }
+
 }
